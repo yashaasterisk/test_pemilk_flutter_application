d0362fd1ae7451893a7e636fb02f036d3f488953
yashaasterisk <yashaasterisk@gmail.com>
2025-06-25 03:49:42 +0700

    perbarui judul aplikasi di main.dart menjadi 'Splash Screen'

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Splash Screen Demo',
+      title: 'Splash Screen',
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
